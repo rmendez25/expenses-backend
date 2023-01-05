49c628a5787a9bf59461496fdb8d7d1d0c758fd3
Robert Mendez <rmendezfamilia@gmail.com>
2023-01-05 17:38:33 -0400

    Fix AuthorityService name

--- src/main/java/com/rmendez/expenses/backend/api/controllers/AuthorityController.java
+++ src/main/java/com/rmendez/expenses/backend/api/controllers/AuthorityController.java
@@ -2,15 +2,16 @@ package com.rmendez.expenses.backend.api.controllers;
 
 import com.rmendez.expenses.backend.api.entities.Authority;
 import com.rmendez.expenses.backend.api.models.AuthorityModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.rmendez.expenses.backend.api.services.AuthorityService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authorities")
+@AllArgsConstructor
 public class AuthorityController {
 
-    @Autowired
-    private AuthorityService authorityService;
+    private final AuthorityService authorityService;
 
     @PostMapping
     public Authority saveAuthority(@RequestBody AuthorityModel authorityModel) {
