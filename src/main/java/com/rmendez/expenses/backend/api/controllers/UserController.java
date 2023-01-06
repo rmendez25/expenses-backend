4121389a4c40d9bbda4da7c75ee0f24acbae0bfc
Robert Mendez <rmendezfamilia@gmail.com>
2023-01-05 20:49:20 -0400

    remove global method authorization from http methods

--- src/main/java/com/rmendez/expenses/backend/api/controllers/UserController.java
+++ src/main/java/com/rmendez/expenses/backend/api/controllers/UserController.java
@@ -6,7 +6,6 @@ import com.rmendez.expenses.backend.api.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -26,27 +25,23 @@ public class UserController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAuthority('write')")
     public Iterable<User> findAllUsers() {
         return userService.findAllUsers();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAuthority('read')")
     private User findUserById(@PathVariable("id") Long id) {
         return userService.findUserById(id);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAuthority('write')")
     public User updateUser(@PathVariable("id") Long id, @RequestBody UserModel userModel) {
         return userService.updateUser();
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PreAuthorize("hasAuthority('write')")
     public ResponseEntity<Object> deleteUser(@PathVariable("id") Long id) {
         Map<String, String> map = new HashMap<>();
         map.put("message", "Deleted Successfully");
@@ -57,7 +52,6 @@ public class UserController {
 
     @PutMapping("/{userId}/authority/{authorityId}")
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAuthority('write')")
     public User setAuthority(@PathVariable("userId") Long userId, @PathVariable("authorityId") Long authorityId) {
         return userService.setAuthority(userId, authorityId);
     }
