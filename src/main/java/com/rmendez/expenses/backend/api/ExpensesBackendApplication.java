92ffcd6473a431c13c0b3f70ce7828fa9c3bf85b
Robert Mendez <rmendezfamilia@gmail.com>
2023-01-19 11:26:54 -0400

    Create User

--- src/main/java/com/rmendez/expenses/backend/api/ExpensesBackendApplication.java
+++ src/main/java/com/rmendez/expenses/backend/api/ExpensesBackendApplication.java
@@ -1,13 +1,25 @@
 package com.rmendez.expenses.backend.api;
 
+import com.rmendez.expenses.backend.api.entities.User;
+import com.rmendez.expenses.backend.api.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ExpensesBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository users, PasswordEncoder encoder) {
+		return args -> {
+			users.save(new User("Robert", "Mendez", "admin", encoder.encode("password")));
+		};
 	}
 
 }
